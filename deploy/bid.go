9d06ab4cbeb5ca99c6050260de0b32cde53cf8e9
ququzone <xueping.yang@gmail.com>
2020-03-28 17:30:02 +0800

    add test for failure case

--- deploy/bid.go
+++ deploy/bid.go
@@ -14,15 +14,12 @@ import (
 )
 
 func main() {
-	b, _ := hexutil.Decode("0x6a242b57227484e904b4e08ba96f19a623c367dcbd18675ec6f2a71a0ff4ec26")
-	fmt.Println(b)
-
 	client, err := rpc.Dial("http://127.0.0.1:8115")
 	if err != nil {
 		log.Fatalf("create rpc client error: %v", err)
 	}
 
-	key, err := secp256k1.HexToKey("9a2b104a02ba9a5959c51368e3c4b38c503c0f519c04d202d4709aeb6a1158f7")
+	key, err := secp256k1.HexToKey("5a59abed0d6fcbfb58b48dcabb4ebae9b91432a3fe1311964bc96b2e410d7da7")
 	if err != nil {
 		log.Fatalf("import private key error: %v", err)
 	}
@@ -35,7 +32,7 @@ func main() {
 	bider, err := key.Script(scripts)
 	pixelID, err := hexutil.Decode("0xcd64ecc1fa2570073cbe9b2dfda7974288b564f323b4cd07e9d84fef22d62661")
 	official, err := hexutil.Decode("0xedcda9513fa030ce4308e29245a22c022d0443bb")
-	owner, err := hexutil.Decode("0xedcda9513fa030ce4308e29245a22c022d0443bb")
+	owner, err := hexutil.Decode("0x69b7667edbe08cf19413102fcadc53c67e34fb71")
 
 	tx := transaction.NewSecp256k1SingleSigTx(scripts)
 
@@ -70,7 +67,8 @@ func main() {
 			Args:     pixelID,
 		},
 	})
-	tx.OutputsData = append(tx.OutputsData, []byte{'0', '0', '1', '2', '3'})
+	tx.OutputsData = append(tx.OutputsData, []byte{'0', '0', 'a', 'b', 'c'})
+
 	// secp256k1
 	tx.Outputs = append(tx.Outputs, &types.CellOutput{
 		Capacity: uint64(6100000000),
@@ -83,7 +81,7 @@ func main() {
 	tx.OutputsData = append(tx.OutputsData, []byte{})
 	// origin pixel lock
 	tx.Outputs = append(tx.Outputs, &types.CellOutput{
-		Capacity: uint64(18000000000),
+		Capacity: uint64(21600000000),
 		Lock: &types.Script{
 			CodeHash: types.HexToHash("0xe959ac726354858d598c9ea1ceb5f617e409b1b0a4a3baa25aa08b6da7b95091"),
 			HashType: types.HashTypeType,
@@ -93,7 +91,7 @@ func main() {
 	tx.OutputsData = append(tx.OutputsData, []byte{})
 	// change
 	tx.Outputs = append(tx.Outputs, &types.CellOutput{
-		Capacity: uint64(767299980000),
+		Capacity: uint64(483582199969000),
 		Lock: bider,
 	})
 	tx.OutputsData = append(tx.OutputsData, []byte{})
@@ -102,7 +100,7 @@ func main() {
 	_, _, err = transaction.AddInputsForTransaction(tx, []*types.Cell{
 		{
 			OutPoint: &types.OutPoint{
-				TxHash: types.HexToHash("0xf23b95472358dd731b71f53dbd19031347a8d1bce8ba4968978ab94d78f6f44a"),
+				TxHash: types.HexToHash("0x2db586414e050c19a5759e714bbbb7f948da7133b91b0ed4660091c42bec4b4b"),
 				Index: 0,
 			},
 		},
@@ -111,8 +109,8 @@ func main() {
 	group, witnessArgs, err := transaction.AddInputsForTransaction(tx, []*types.Cell{
 		{
 			OutPoint: &types.OutPoint{
-				TxHash: types.HexToHash("0x59f1fca75408ea690c6055022ccfa8961e782cbfff16ce083fd15216191543e3"),
-				Index: 3,
+				TxHash: types.HexToHash("0x3615307e4f8e435113a53bf0500d34e4c4046db2b7877ba26a1955adc206d7ff"),
+				Index: 1,
 			},
 		},
 	})
